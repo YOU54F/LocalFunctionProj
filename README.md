902eb9971a868273dd7cdbb21c1eec64deb75799
Yousaf Nabi <yousaf.nabi@smartbear.com>
2022-10-15 04:29:54 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # action-wf-test
+
+1. `func init LocalFunctionProj --dotnet`
+1. `cd LocalFunctionProj`
+1. `func new --name HttpExample --template "HTTP trigger" --authlevel "anonymous"`
+1. `func start`
+
+2. `mkdir LocalFunctionProjTest`
+3. `chmod ugo+rwx LocalFunctionProjTest`
+4. Create ReadyAPI Test Suite and store in `LocalFunctionProjTest`
+
+5. export `SLM_API_KEY`
+6. `./startServerAndTest.sh`
---
+++ startServerAndTest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SERVER_DIR=${SERVER_DIR:-'LocalFunctionProj'}
+SERVER_COMMAND=${SERVER_COMMAND:-'func start'}
+WAIT_FOR=http://localhost:7071
+
+BASE_DIR=$PWD
+SERVER_DIR=${SERVER_DIR:-$PWD}
+
+start_server() {
+    echo "Start server ..."
+    cd $SERVER_DIR
+    $SERVER_COMMAND &
+    server_pid=$! && cd $BASE_DIR
+}
+
+stop_server() {
+    echo "Stop Server ...$server_pid"
+    kill -9 $server_pid
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+start_server
+
+./wait-for $WAIT_FOR -- ./launchReadyAPITestRunner.sh
+
+stop_server
